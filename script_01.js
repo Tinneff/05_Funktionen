1811a97fa947760cf3564a706e7bddce46858e2e
Peter <schmidt.multimedia@gmail.com>
2023-01-17 10:24:50 +0100

    1. Kapselung von Codeblöcken

--- script_01.js
+++ script_01.js
@@ -1,6 +1,14 @@
 "use strict";
 
 /***** Funktionen 01 *****/
-
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf (call)
+test();
+
+// Funktionsrumpf (body) | callee
+// Funktionsdeklaration
+function test()
+{
+    console.log("Hallo Patrick!");
+}
